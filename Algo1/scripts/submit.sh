692cd5ad4ad42baca24a457381a24799a78ec623
ppoeung <35326799+ppoeung@users.noreply.github.com>
2018-02-12 18:15:48 +0100

    Ajout submit.sh

--- Algo1/scripts/submit.sh
+++ Algo1/scripts/submit.sh
@@ -8,6 +8,7 @@
     send "echo '"$1"'/lib/*.jar | tr \" \" \",\"\r"
     expect -re {.*\r\n([_a-zA-Z0-9\-\/\.,?]*)\r\n} {set files $expect_out(1,string)}
     expect "$ "
+    send "spark-submit --master yarn --conf spark.executorEnv.JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk/ --class algo1_worksheet.test --jars $files '"$1"'/lib/sparksenti_2.11-0.1.jar '"$4"' \r"
     expect "$ "
     close
 '
